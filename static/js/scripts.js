e710cd4f39513b96aa57634bce3dbda5b1920277
Emily Lau <emily.lau@drake.edu>
2025-04-30 19:52:45 +0000

    Updated scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,24 @@
-sfsdg
+
+// Javascript for the theme toggle (dark/light mode)
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+          
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            themeToggle.checked = true;
+        }
+    }
+
+    themeToggle.addEventListener('change', function() {
+        if (themeToggle.checked) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.removeItem('theme');
+        }
+    });
+});
